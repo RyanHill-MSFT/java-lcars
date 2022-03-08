8ed72c2ecd6de97b88005ed73d97a81720f250ab
Ryan Hill <hill.ryan@microsoft.com>
2022-03-08 17:34:03 -0500

    Initial Commit

---
+++ lcars-webpubsub-chat/src/main/java/com/lcars/webpubsub/App.java
@@ -0,0 +1,13 @@
+package com.lcars.webpubsub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
